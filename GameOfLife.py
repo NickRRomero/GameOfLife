fd8c8be82b99d03cab3173ac005651835ba0a442
NickRRomero <nromo01@gmail.com>
2016-03-22 17:21:14 -0700

    Function created to retrieve user input

---
+++ GameOfLife.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import numpy as np
+import random
+
+def get_user_input():
+   print """Welcome to The Game Of Life.\n""" 
+   print """Please enter X for the size of the game board: X * X\n"""
+   return input('Size: ')
+
+
+
+
+
+if __name__ == "__main__":
+   board_size = get_user_input()
