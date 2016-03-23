3622321e806556428ad671ceeb9a4d6ca1550924
NickRRomero <nromo01@gmail.com>
2016-03-22 17:35:54 -0700

    Added function to initialize board

--- GameOfLife.py
+++ GameOfLife.py
@@ -7,9 +7,11 @@ def get_user_input():
    print """Please enter X for the size of the game board: X * X\n"""
    return input('Size: ')
 
-
+def create_empty_board(board_size):
+   return np.zeros((board_size, board_size), dtype=np.int8, order='C')
 
 
 
 if __name__ == "__main__":
    board_size = get_user_input()
+   print create_empty_board(board_size)
